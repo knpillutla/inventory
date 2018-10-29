5550429cf27c5c20a562ab8add1c62d5dd5c7ceb
Krishna Pillutla <knpillutla@gmail.com>
2018-10-29 17:59:34 -0400

    Adding Content

--- src/main/java/com/threedsoft/inventory/endpoint/rest/InventoryRestEndPoint.java
+++ src/main/java/com/threedsoft/inventory/endpoint/rest/InventoryRestEndPoint.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,100 +22,122 @@ import com.threedsoft.inventory.dto.requests.InventoryCreationRequestDTO;
 import com.threedsoft.inventory.dto.requests.InventoryUpdateRequestDTO;
 import com.threedsoft.inventory.exception.InventoryException;
 import com.threedsoft.inventory.service.InventoryServiceByItem;
+import com.threedsoft.util.dto.ErrorResourceDTO;
 
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/inventory/v1")
-@Api(value="Inventory Service", description="Operations pertaining to Inventory")
+@Api(value = "Inventory Service", description = "Operations pertaining to Inventory")
 @RefreshScope
 @Slf4j
 public class InventoryRestEndPoint {
 
-    @Autowired
-    InventoryServiceByItem invnService;
-	
-    @Value("${wms.service.health.msg: Inventory Service - Config Server is not working..please check}")
-    private String healthMsg;
-    
-    @Value("${wms.service.ready.msg: Inventory Service - Not ready yet}")
-    private String readyMsg;
+	@Autowired
+	InventoryServiceByItem invnService;
+
+	@Value("${wms.service.health.msg: Inventory Service - Config Server is not working..please check}")
+	private String healthMsg;
+
+	@Value("${wms.service.ready.msg: Inventory Service - Not ready yet}")
+	private String readyMsg;
 
 	@GetMapping("/ready")
 	public ResponseEntity ready() throws Exception {
 		return ResponseEntity.ok(readyMsg);
 	}
-	
+
 	@GetMapping("/health")
 	public ResponseEntity health() throws Exception {
 		return ResponseEntity.ok(healthMsg);
 	}
-	
+
 	@GetMapping("/{busName}/{locnNbr}/inventory/{id}")
-	public ResponseEntity getById(@PathVariable("locnNbr") Integer locnNbr, @PathVariable("id") Long id) throws IOException {
+	public ResponseEntity getById(@PathVariable("locnNbr") Integer locnNbr, @PathVariable("id") Long id)
+			throws IOException {
 		try {
 			return ResponseEntity.ok(invnService.findById(locnNbr, id));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return ResponseEntity.badRequest().body(new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error occured while getting next pick task"));
+			return ResponseEntity.badRequest().body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					"Error occured while getting inventory for id:" + id + ":" + e.getMessage()));
 		}
 	}
 
 	@GetMapping("/{busName}/{locnNbr}/inventory")
-	public ResponseEntity getInventoryList(@PathVariable("busName") String busName, @PathVariable("locnNbr") Integer locnNbr) throws IOException {
+	public ResponseEntity getInventoryList(@PathVariable("busName") String busName,
+			@PathVariable("locnNbr") Integer locnNbr) throws IOException {
 		try {
 			return ResponseEntity.ok(invnService.findByBusNameAndLocnNbr(busName, locnNbr));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return ResponseEntity.badRequest().body(new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error occured while getting next pick task"));
+			return ResponseEntity.badRequest()
+					.body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+							"Error occured while getting inventory  list for busName:" + busName + ",locnNbr:" + locnNbr
+									+ ":" + e.getMessage()));
 		}
 	}
 
 	@PostMapping("/{busName}/{locnNbr}/inventory/reserve/{id}")
-	public ResponseEntity reserveInventory(@PathVariable("busName") String busName, @PathVariable("locnNbr") Integer locnNbr, @RequestBody InventoryAllocationRequestDTO invnAllocationReq) throws IOException {
+	public ResponseEntity reserveInventory(@PathVariable("busName") String busName,
+			@PathVariable("locnNbr") Integer locnNbr, @RequestBody InventoryAllocationRequestDTO invnAllocationReq)
+			throws IOException {
 		try {
 			return ResponseEntity.ok(invnService.allocateInventory(invnAllocationReq));
-		}catch (InventoryException ex) {
+		} catch (InventoryException ex) {
 			log.error("ReserveInventory Error:", ex.getEvent(), ex);
-			return ResponseEntity.badRequest().body(ex.getEvent());
-		} 
-	}	
-	
+			return ResponseEntity.badRequest().body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					"Error while reserving inventory for busName:" + busName + ",locnNbr:" + locnNbr
+					+ ":" + ex.getMessage()));
+		}
+	}
+
 	@PostMapping("/{busName}/{locnNbr}/inventory/{id}")
-	public ResponseEntity updateInventory(@PathVariable("busName") String busName, @PathVariable("locnNbr") Integer locnNbr, @RequestBody InventoryUpdateRequestDTO invnUpdateReq) throws IOException {
+	public ResponseEntity updateInventory(@PathVariable("busName") String busName,
+			@PathVariable("locnNbr") Integer locnNbr, @RequestBody InventoryUpdateRequestDTO invnUpdateReq)
+			throws IOException {
 		try {
 			return ResponseEntity.ok(invnService.updateInventory(invnUpdateReq));
-		}catch (InventoryException ex) {
+		} catch (InventoryException ex) {
 			log.error("Inventory Update Error:", ex.getEvent(), ex);
-			return ResponseEntity.badRequest().body(new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error occured while updating inventory:" + ex.getMessage()));
-		} 
-	}	
+			return ResponseEntity.badRequest().body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					"Error occured while updating inventory:" + ex.getMessage(), invnUpdateReq));
+		}
+	}
 
 	@DeleteMapping("/{busName}/{locnNbr}/inventory/{id}")
-	public ResponseEntity deleteInventory(@PathVariable("busName") String busName, @PathVariable("locnNbr") Integer locnNbr, @PathVariable("id") Long id) throws IOException {
+	public ResponseEntity deleteInventory(@PathVariable("busName") String busName,
+			@PathVariable("locnNbr") Integer locnNbr, @PathVariable("id") Long id) throws IOException {
 		try {
 			return ResponseEntity.ok(invnService.deleteInventory(id));
-		}catch (InventoryException ex) {
-			log.error("Inventory Update Error:", ex.getEvent(), ex);
-			return ResponseEntity.badRequest().body(new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error occured while updating inventory:" + ex.getMessage()));
-		} 
-	}	
+		} catch (InventoryException ex) {
+			log.error("Inventory Delete Error:", ex.getEvent(), ex);
+			return ResponseEntity.badRequest().body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					"Error occured while deleting inventory:" + ex.getMessage()));
+		}
+	}
 
 	@PostMapping("/{busName}/{locnNbr}/inventory")
-	public ResponseEntity createInventory(@PathVariable("locnNbr") Integer locnNbr, @RequestBody InventoryCreationRequestDTO invnCreationReq) throws IOException {
+	public ResponseEntity createInventory(@PathVariable("locnNbr") Integer locnNbr,
+			@RequestBody InventoryCreationRequestDTO invnCreationReq) throws IOException {
 		long startTime = System.currentTimeMillis();
-		log.info("Received Inventory Create request for : " + invnCreationReq.toString() + ": at :" + LocalDateTime.now());
+		log.info("Received Inventory Create request for : " + invnCreationReq.toString() + ": at :"
+				+ LocalDateTime.now());
 		ResponseEntity resEntity = null;
 		try {
-			resEntity = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(invnService.createInventory(invnCreationReq));
+			resEntity = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
+					.body(invnService.createInventory(invnCreationReq));
 		} catch (InventoryException ex) {
 			log.error("CreateInventory Error:", ex.getEvent(), ex);
-			resEntity = ResponseEntity.badRequest().body(ex.getEvent());
+			resEntity = ResponseEntity.badRequest().body(new ErrorResourceDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					"Error occured while deleting inventory:" + ex.getMessage(), invnCreationReq));
 		}
 		long endTime = System.currentTimeMillis();
-		log.info("Completed Inventory Create request for : " + invnCreationReq.toString() + ": at :" + LocalDateTime.now() + " : total time:" + (endTime-startTime)/1000.00 + " secs");
+		log.info("Completed Inventory Create request for : " + invnCreationReq.toString() + ": at :"
+				+ LocalDateTime.now() + " : total time:" + (endTime - startTime) / 1000.00 + " secs");
 		return resEntity;
-	}	
+	}
 }
